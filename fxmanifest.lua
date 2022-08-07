19783019b8df8af1af1cf4cf9be6743f18f554e0
RT <50390909+RT-XENON@users.noreply.github.com>
2022-08-07 19:15:35 +0700

    Create Github

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+
+game { "gta5" }
+
+ui_page "interface/interface.html"
+
+files {
+    "interface/interface.html",
+    "interface/assistant/*.png",
+    "interface/assistant/*.jpg",
+    "interface/config/color.css",
+    "interface/lib/css/*.css",
+    "interface/lib/js/*.js",
+}
